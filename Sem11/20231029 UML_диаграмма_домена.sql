681651aff8c2689dc206454cfc6f91c6b0ffe704
Mipusatina <mipspam2@mail.ru>
2023-11-06 00:01:35 +0100

    Sem 11. DZ

---
+++ Sem11/20231029 UML_диаграмма_домена.sql
@@ -0,0 +1,20 @@
+ALTER TABLE `Пароль` DROP FOREIGN KEY `Пароль_fk0`;
+
+ALTER TABLE `Платежные` DROP FOREIGN KEY `Платежные_fk0`;
+
+ALTER TABLE `Отстатки` DROP FOREIGN KEY `Отстатки_fk0`;
+
+ALTER TABLE `Отстатки` DROP FOREIGN KEY `Отстатки_fk1`;
+
+ALTER TABLE `Цены` DROP FOREIGN KEY `Цены_fk0`;
+
+ALTER TABLE `Работник` DROP FOREIGN KEY `Работник_fk0`;
+
+ALTER TABLE `Заказы` DROP FOREIGN KEY `Заказы_fk0`;
+
+ALTER TABLE `Заказы` DROP FOREIGN KEY `Заказы_fk1`;
+
+ALTER TABLE `ПозицииЗаказа` DROP FOREIGN KEY `ПозицииЗаказа_fk0`;
+
+ALTER TABLE `ПозицииЗаказа` DROP FOREIGN KEY `ПозицииЗаказа_fk1`;
+
